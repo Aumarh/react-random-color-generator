63d623512f118f1ebea20b84b8ca23cd311d62a8
Aumarh <aumarh@riseup.net>
2022-05-09 18:13:59 +0200

    Add first working version

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import RandomColor from './RandomColor';
+
+// import { useState } from 'react';
+// import EventHandlers from './EventHandlers';
+
+export default function App() {
+  return <RandomColor />;
+}
